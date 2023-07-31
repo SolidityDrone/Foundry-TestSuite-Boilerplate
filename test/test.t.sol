51fe780a4e88bf06475791ffae87baaa58a438f6
SolidityDrone <pnn.claudoio@gmail.com>
2023-07-31 16:14:24 +0200

    Your commit message here

---
+++ test/test.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+
+import "std/test.sol";
+import "../src/contract.sol";
+
+contract Test_Sample is Test, IERC721Receiver {
+
+
+    function setUp() public {
+    
+    }
+    
+}
