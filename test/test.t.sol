c46e53c11b56de23aed91d71572d74106de70315
SolidityDrone <104315978+SolidityDrone@users.noreply.github.com>
2023-11-07 15:20:38 +0100

    Update test.t.sol

--- test/test.t.sol
+++ test/test.t.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.6;
 import "std/test.sol";
 import "../src/contract.sol";
 
-contract Test_Sample is Test, IERC721Receiver {
+contract Test_Sample is Test {
 
 
     function setUp() public {
